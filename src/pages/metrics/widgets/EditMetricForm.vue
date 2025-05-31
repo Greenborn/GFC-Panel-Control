0d088dd09561d716fd4c39dd4b3b3b12b4929f41
luciano <lucho.2012.tandil@gmail.com>
2025-05-31 04:27:19 -0300

    [#241] El formulario ahora tiene los campos necesarios

--- src/pages/metrics/widgets/EditMetricForm.vue
+++ src/pages/metrics/widgets/EditMetricForm.vue
@@ -1,22 +1,21 @@
-<script setup>
-import { ref } from "vue";
-import ProjectStatusBadge from "../components/ProjectStatusBadge.vue"
-
-import { DEFAULT_METRIC } from "../const.js"
-
-const props = defineProps(["metric"])
-
-const newProject = ref({ ...DEFAULT_METRIC })
-
-const required = (v) => !!v || "This field is required"
-
-</script>
-
 <template>
   <VaForm v-slot="{ validate }" class="flex flex-col gap-2">
     <VaInput
-      v-model="newProject.project_name"
-      label="Project name"
+      v-for="(field, index) in FIELDS_METRIC.filter((field) => field.key !== 'actions' && field.key !== 'organization_type')"
+      :key="index"
+      v-model="newMetric[field.key]"
+      :label="field.label"
+      :rules="[required]"
+      :type="field.key === 'score' ? 'number' : 'text'"
+    />
+    <VaSelect
+      v-model="newMetric.organization_type"
+      label="Tipo Concurso"
+      :options="[
+        { value: 'INTERNO', text: 'Interno' },
+        { value: 'EXTERNO', text: 'Externo' },
+        { value: 'EXTERNO_UNICEN', text: 'Externo Unicen' }
+      ]"
       :rules="[required]"
     />
     
@@ -24,10 +23,25 @@ const required = (v) => !!v || "This field is required"
       <VaButton preset="secondary" color="secondary" @click="$emit('close')"
         >Cancelar</VaButton
       >
-      <VaButton @click="validate() && $emit('save', newProject)">Guardar</VaButton>
+      <VaButton @click="validate() && $emit('save', newMetric)">Guardar</VaButton>
     </div>
   </VaForm>
 </template>
+
+<script setup>
+import { ref } from "vue"
+import { FIELDS_METRIC } from "../const.js"
+import { DEFAULT_METRIC } from "../const.js"
+
+const props = defineProps(["metric"])
+const emit = defineEmits(["close", "save"])
+
+const newMetric = ref(
+  props?.metric ? props?.metric : { ...DEFAULT_METRIC, organization_type: 'INTERNO' }
+)
+
+const required = (v) => !!v || "This field is required"
+</script>
 
 <style lang="scss" scoped>
 .va-select-content__autocomplete {
